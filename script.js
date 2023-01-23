3a6dfebc55051f65685eebbb99459bafc5e1280b
Kyushik <kyushik95@gmail.com>
2023-01-23 17:53:55 -0500

    nav bar complete

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementsByClassName(`toggle-button`)[0]
+const navBarLinks = document.getElementsByClassName(`navBar-links`)[0]
+
+toggleButton.addEventListener(`click`,() => {
+    navBarLinks.classList.toggle(`active`)
+})
